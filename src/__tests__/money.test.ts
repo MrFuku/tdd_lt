01e30398f3b4ee8a62a81119ae0e350807ce7695
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2021-12-11 15:09:12 +0900

    :sparkles: Money から派生した Franc と Dollar は同じはずなのでそれの比較を追加する
    ・Money クラスで equals は amount の比較を行っているが TypeScript が Cast を行ってくれているため、動作するようになっている（Javaだと落ちる

--- src/__tests__/money.test.ts
+++ src/__tests__/money.test.ts
@@ -1,3 +1,5 @@
+import { Dollar } from '../dollar';
+import { Franc } from '../franc';
 import { Money } from '../money';
 
 test('equals', () => {
@@ -9,4 +11,9 @@ test('null equals', () => {
   const five = new Money(5);
 
   expect(five.equals(null)).toBeFalsy();
+});
+
+test('equals Franc = Dollar', () => {
+  expect(new Franc(5).equals(new Dollar(5))).toBeTruthy();
+  expect(new Franc(5).equals(new Dollar(6))).toBeFalsy();
 });
