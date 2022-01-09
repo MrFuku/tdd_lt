7fd208bfd55b7d414dc64d218f1d91ecc215d216
MrFuku <mmafighter_shun@icloud.com>
2022-01-09 16:04:58 +0900

    Done テストコードのリファクタリング

--- src/__tests__/money.test.ts
+++ src/__tests__/money.test.ts
@@ -39,6 +39,18 @@ describe("Money", () => {
       expect(Money.dollar(1).currency).toBe("USD");
       expect(Money.franc(1).currency).toBe("CHF");
     });
+
+    test("同じ通貨同士の加算の戻り値はMoneyオブジェクトになる", () => {
+      const fiveBucks = Money.dollar(5);
+      const resultBucks = fiveBucks.plus(fiveBucks) as Money;
+      expect(resultBucks).toBeInstanceOf(Money);
+      expect(resultBucks.currency).toBe(fiveBucks.currency);
+
+      const fiveFrancs = Money.franc(5);
+      const resultFrancs = fiveFrancs.plus(fiveFrancs) as Money;
+      expect(resultFrancs).toBeInstanceOf(Money);
+      expect(resultFrancs.currency).toBe(fiveFrancs.currency);
+    });
   });
 });
 
@@ -59,71 +71,61 @@ describe("通貨の計算ができる", () => {
     const reducedDecimal = bank.reduce(sumDecimal, "USD");
     expect(reducedDecimal).toEqual(Money.dollar(0.9485));
   });
+
+  test("整数の掛け算ができる", () => {
+    const fiveBucks: Money = Money.dollar(5);
+    const fiveFrancs: Money = Money.franc(5);
+
+    expect(fiveBucks.times(2)).toEqual(Money.dollar(10));
+    expect(fiveBucks.times(3)).toEqual(Money.dollar(15));
+    expect(fiveFrancs.times(2)).toEqual(Money.franc(10));
+    expect(fiveFrancs.times(3)).toEqual(Money.franc(15));
+  });
+
+  test("異なる通貨間の3回以上の計算", () => {
+    const fiveBucks: Expression = Money.dollar(5);
+    const tenFrancs: Expression = Money.franc(10);
+    const bank = new Bank();
+    bank.addRate("CHF", "USD", 2);
+
+    const sum: Expression = new Sum(fiveBucks, tenFrancs).plus(fiveBucks);
+    const result = bank.reduce(sum, "USD");
+    expect(result).toEqual(Money.dollar(15));
+  });
+
+  test("加算と乗算を組み合わせた計算ができる", () => {
+    const fiveBucks: Expression = Money.dollar(5);
+    const tenFrancs: Expression = Money.franc(10);
+    const bank = new Bank();
+    bank.addRate("CHF", "USD", 2);
+
+    const sum: Expression = new Sum(fiveBucks, tenFrancs).times(2);
+    const result = bank.reduce(sum, "USD");
+    expect(result).toEqual(Money.dollar(20));
+  });
 });
 
-test("times", () => {
-  const fiveBucks: Money = Money.dollar(5);
-  const fiveFrancs: Money = Money.franc(5);
+describe("為替", () => {
+  test("同じ通貨間のレートは1", () => {
+    const bank = new Bank();
+    const rate = bank.rate("USD", "USD");
 
-  expect(fiveBucks.times(2)).toEqual(Money.dollar(10));
-  expect(fiveBucks.times(3)).toEqual(Money.dollar(15));
-  expect(fiveFrancs.times(2)).toEqual(Money.franc(10));
-  expect(fiveFrancs.times(3)).toEqual(Money.franc(15));
-});
+    expect(1).toEqual(rate);
+  });
 
-test("test plus returns money when same currency", () => {
-  const fiveBucks = Money.dollar(5);
-  const resultBucks = fiveBucks.plus(fiveBucks) as Money;
-  expect(resultBucks).toBeInstanceOf(Money);
-  expect(resultBucks.currency).toBe(fiveBucks.currency);
+  test("適切なレートで為替される", () => {
+    const bank = new Bank();
+    bank.addRate("CHF", "USD", 2);
+    const result = bank.reduce(Money.franc(2), "USD");
+    expect(Money.dollar(1)).toEqual(result);
+  });
 
-  const fiveFrancs = Money.franc(5);
-  const resultFrancs = fiveFrancs.plus(fiveFrancs) as Money;
-  expect(resultFrancs).toBeInstanceOf(Money);
-  expect(resultFrancs.currency).toBe(fiveFrancs.currency);
-});
-
-test("test identity rate", () => {
-  const bank = new Bank();
-  const rate = bank.rate("USD", "USD");
-
-  expect(1).toEqual(rate);
-});
-
-test("test reduce money different currency", () => {
-  const bank = new Bank();
-  bank.addRate("CHF", "USD", 2);
-  const result = bank.reduce(Money.franc(2), "USD");
-  expect(Money.dollar(1)).toEqual(result);
-});
-
-test("test mixed addition", () => {
-  const fiveBucks: Expression = Money.dollar(5);
-  const tenFrancs: Expression = Money.franc(10);
-  const bank = new Bank();
-  bank.addRate("CHF", "USD", 2);
-  const result = bank.reduce(fiveBucks.plus(tenFrancs), "USD");
-  expect(result).toEqual(Money.dollar(10));
-});
-
-test("test sum plus money", () => {
-  const fiveBucks: Expression = Money.dollar(5);
-  const tenFrancs: Expression = Money.franc(10);
-  const bank = new Bank();
-  bank.addRate("CHF", "USD", 2);
-
-  const sum: Expression = new Sum(fiveBucks, tenFrancs).plus(fiveBucks);
-  const result = bank.reduce(sum, "USD");
-  expect(result).toEqual(Money.dollar(15));
-});
-
-test("test sum times", () => {
-  const fiveBucks: Expression = Money.dollar(5);
-  const tenFrancs: Expression = Money.franc(10);
-  const bank = new Bank();
-  bank.addRate("CHF", "USD", 2);
-
-  const sum: Expression = new Sum(fiveBucks, tenFrancs).times(2);
-  const result = bank.reduce(sum, "USD");
-  expect(result).toEqual(Money.dollar(20));
+  test("異なる通貨の加算においてレートを考慮して計算できる", () => {
+    const fiveBucks: Expression = Money.dollar(5);
+    const tenFrancs: Expression = Money.franc(10);
+    const bank = new Bank();
+    bank.addRate("CHF", "USD", 2);
+    const result = bank.reduce(fiveBucks.plus(tenFrancs), "USD");
+    expect(result).toEqual(Money.dollar(10));
+  });
 });
